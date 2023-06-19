c65d2b014ecc2c60e8c63058e99525dbc9428fc8
victor <vorellana99@hotmail.com>
2023-06-19 05:16:26 -0500

    Implementation of challenge 2

--- 02-nodejs/index.js
+++ 02-nodejs/index.js
@@ -1,12 +1,40 @@
 'use strict'
 
 const express = require('express')
-
 const User = require('./models/User')
+const csvWriter = require('csv-write-stream')
 
 // Setup Express.js app
 const app = express()
 
-// TODO: everything else
+app.get('/users', (req, res) => {
+  try {
+    // Create a read stream from the database
+    const userStream = User.find().cursor()
+
+    // Set up the response headers to inform the client that we are sending a CSV file
+    res.setHeader('Content-Type', 'text/csv')
+    res.setHeader('Content-Disposition', 'attachment; filename=users.csv')
+
+    // Create CSV stream with csv-write-stream
+    const writer = csvWriter({ headers: ['id', 'name', 'email'] })
+    writer.pipe(res)
+
+    // Write each user obtained from the stream to the CSV file
+    userStream.on('data', (user) => {
+      writer.write({
+        id: user._id,
+        name: user.name,
+        email: user.email
+      })
+    })
+
+    // Ends writing to the CSV file when all users have been processed
+    userStream.on('end', () => writer.end())
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('There was an error trying to obtain the users')
+  }
+})
 
 app.listen(3000)
